8ef10a17f2034994ed50710d076464401544f4c8
Ramon Perez <rapega12@gmail.com>
2020-01-29 11:48:03 +0100

    now plots are passed through an url instead of being saved (more efficient and clean)

--- visual/graphic.py
+++ visual/graphic.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, redirect, render_template, request, url_for
+    Blueprint, redirect, render_template, request, url_for, send_file
 )
 
 #--------------------- Imports to generate the plot ---------------------
@@ -14,223 +14,91 @@ from random import randint
 from pandas.plotting import register_matplotlib_converters
 from os import remove
 from matplotlib.dates import num2date
+from io import BytesIO
 
 #The number of simulations run to generate the ci
 #A value too high will make the script go slow
 n_sim = 101
-sns.set_style('whitegrid')
 #------------------------------------------------------------------------
+
+
+def get_form(form):
+    start_date = form['pred_from']
+    months = form['pred_to']
+    min_ci = form['low_perc']
+    max_ci = form['high_perc']
+    return start_date, months, min_ci, max_ci
+
+
+def get_args(args):
+    months = args.get('start')
+    start_date = args.get('month')
+    min_ci = args.get('minci')
+    max_ci = args.get('maxci')
+
+    try:
+        # We only chekc for type compatibility as they would be
+        # converted again to str when passed as arguments in the
+        # template
+        datetime.strptime(start_date, '%Y-%m-%d')
+        int(min_ci)
+        int(max_ci)
+        return start_date, months, min_ci, max_ci
+
+    except:
+        return None
+
 
 bp = Blueprint('graphic', __name__)
 
 @bp.route('/', methods=('GET', 'POST'))
 @bp.route('/bitcoin', methods=('GET', 'POST'))
-def index():
+def bitcoin():
     if request.method == 'POST':
-        start_date = request.form['pred_from']
-        months = request.form['pred_to']
-        min_ci = request.form['low_perc']
-        max_ci = request.form['high_perc']
-
-        parameters = (start_date, months, [min_ci, max_ci])
-        return redirect(url_for('index', month=months, start=start_date, minci=min_ci, maxci=max_ci))
+        form = get_form(request.form)
+        return redirect(url_for('graphic.bitcoin', month=form[0], start=form[1], minci=form[2], maxci=form[3]))
     
-    months = request.args.get('month')
-    start_date = request.args.get('start')
-    min_ci = request.args.get('minci')
-    max_ci = request.args.get('maxci')
-
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        months = int(months)
-        min_ci = int(min_ci)
-        max_ci = int(max_ci)
-        params = (start_date, months, [min_ci, max_ci])
-    except:
-        params = (None,)
-
-    #Create plot
-    if all(params):
-        fig_code = plot_prediction('bitcoin', *params)
-        fig_path = '{:012d}'.format(fig_code) + '.png'
-        return render_template('cryptos/bitcoin.html', image_url='/static/images/temp/' + fig_path)
-    else:
-        return render_template('cryptos/bitcoin.html', image_url='/static/images/plots/default_bitcoin.png')
+    return render_template('cryptos/bitcoin.html', params=get_args(request.args))
 
 
 @bp.route('/ethereum', methods=('GET', 'POST'))
 def ethereum():
     if request.method == 'POST':
-        start_date = request.form['pred_from']
-        months = request.form['pred_to']
-        min_ci = request.form['low_perc']
-        max_ci = request.form['high_perc']
-
-        parameters = (start_date, months, [min_ci, max_ci])
-        return redirect(url_for('graphic.ethereum', month=months, start=start_date, minci=min_ci, maxci=max_ci))
-
-    months = request.args.get('month')
-    start_date = request.args.get('start')
-    min_ci = request.args.get('minci')
-    max_ci = request.args.get('maxci')
-
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        months = int(months)
-        min_ci = int(min_ci)
-        max_ci = int(max_ci)
-        params = (start_date, months, [min_ci, max_ci])
-    except:
-        params = (None,)
-
-    #Create plot
-    if all(params):
-        fig_code = plot_prediction('ethereum', *params)
-        fig_path = '{:012d}'.format(fig_code) + '.png'
-        return render_template('cryptos/ethereum.html', image_url='/static/images/temp/' + fig_path)
-    else:
-        return render_template('cryptos/ethereum.html', image_url='/static/images/plots/default_ethereum.png')
+        form = get_form(request.form)
+        return redirect(url_for('graphic.ethereum', month=form[0], start=form[1], minci=form[2], maxci=form[3]))
+    
+    return render_template('cryptos/ethereum.html', params=get_args(request.args))
 
 
 @bp.route('/ripple', methods=('GET', 'POST'))
 def ripple():
     if request.method == 'POST':
-        start_date = request.form['pred_from']
-        months = request.form['pred_to']
-        min_ci = request.form['low_perc']
-        max_ci = request.form['high_perc']
-
-        parameters = (start_date, months, [min_ci, max_ci])
-        return redirect(url_for('graphic.ripple', month=months, start=start_date, minci=min_ci, maxci=max_ci))
-
-    months = request.args.get('month')
-    start_date = request.args.get('start')
-    min_ci = request.args.get('minci')
-    max_ci = request.args.get('maxci')
-
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        months = int(months)
-        min_ci = int(min_ci)
-        max_ci = int(max_ci)
-        params = (start_date, months, [min_ci, max_ci])
-    except:
-        params = (None,)
-
-    #Create plot
-    if all(params):
-        fig_code = plot_prediction('ripple', *params)
-        fig_path = '{:012d}'.format(fig_code) + '.png'
-        return render_template('cryptos/ripple.html', image_url='/static/images/temp/' + fig_path)
-    else:
-        return render_template('cryptos/ripple.html', image_url='/static/images/plots/ripple_default.png')
+        form = get_form(request.form)
+        return redirect(url_for('graphic.ripple', month=form[0], start=form[1], minci=form[2], maxci=form[3]))
+    
+    return render_template('cryptos/ripple.html', params=get_args(request.args))
 
 
 @bp.route('/bitcoin-cash', methods=('GET', 'POST'))
 def bitcoinCash():
     if request.method == 'POST':
-        start_date = request.form['pred_from']
-        months = request.form['pred_to']
-        min_ci = request.form['low_perc']
-        max_ci = request.form['high_perc']
-
-        parameters = (start_date, months, [min_ci, max_ci])
-        return redirect(url_for('graphic.bitcoinCash', month=months, start=start_date, minci=min_ci, maxci=max_ci))
-
-    months = request.args.get('month')
-    start_date = request.args.get('start')
-    min_ci = request.args.get('minci')
-    max_ci = request.args.get('maxci')
-
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        months = int(months)
-        min_ci = int(min_ci)
-        max_ci = int(max_ci)
-        params = (start_date, months, [min_ci, max_ci])
-    except:
-        params = (None,)
-
-    #Create plot
-    if all(params):
-        fig_code = plot_prediction('bitcoin-cash', *params)
-        fig_path = '{:012d}'.format(fig_code) + '.png'
-        return render_template('cryptos/bitcoin-cash.html', image_url='/static/images/temp/' + fig_path)
-    else:
-        return render_template('cryptos/bitcoin-cash.html', image_url='/static/images/plots/bcash_default.png')
+        form = get_form(request.form)
+        return redirect(url_for('graphic.bitcoinCash', month=form[0], start=form[1], minci=form[2], maxci=form[3]))
+    
+    return render_template('cryptos/bitcoin-cash.html', params=get_args(request.args))
 
 
 @bp.route('/bitcoin-sv', methods=('GET', 'POST'))
 def bitcoinSV():
     if request.method == 'POST':
-        start_date = request.form['pred_from']
-        months = request.form['pred_to']
-        min_ci = request.form['low_perc']
-        max_ci = request.form['high_perc']
-
-        parameters = (start_date, months, [min_ci, max_ci])
-        return redirect(url_for('graphic.bitcoinSV', month=months, start=start_date, minci=min_ci, maxci=max_ci))
-
-    months = request.args.get('month')
-    start_date = request.args.get('start')
-    min_ci = request.args.get('minci')
-    max_ci = request.args.get('maxci')
-
-    try:
-        start_date = datetime.strptime(start_date, '%Y-%m-%d')
-        months = int(months)
-        min_ci = int(min_ci)
-        max_ci = int(max_ci)
-        params = (start_date, months, [min_ci, max_ci])
-    except:
-        params = (None,)
-
-    #Create plot
-    if all(params):
-        fig_code = plot_prediction('bitcoin-sv', *params)
-        fig_path = '{:012d}'.format(fig_code) + '.png'
-        return render_template('cryptos/bitcoin-sv.html', image_url='/static/images/temp/' + fig_path)
-    else:
-        return render_template('cryptos/bitcoin-sv.html', image_url='/static/images/plots/bsv_default.png')
-
-
-#--------------------- Functions to generate the plot ---------------------
-
-def GenerateFakeResults(original, results):
-    """Generates fake results from fake data.
-    Fake data is generated from mixing the resiudals of the fitted model.
-    Then the model is trained on this fake data.
+        form = get_form(request.form)
+        return redirect(url_for('graphic.bitcoinSV', month=form[0], start=form[1], minci=form[2], maxci=form[3]))
     
-    original: data used to generate the original results
-    results: a fitted least squares model
-    
-    returns: a fitted least squares model
-    """
-    fake = pd.DataFrame(original.num_date.copy())
-    fake['close'] = results.fittedvalues + np.random.choice(results.resid, len(results.resid))
-    formula = "close ~ num_date"
-    model = smf.ols(formula, data=fake)
-    return model.fit()
+    return render_template('cryptos/bitcoin-sv.html', params=get_args(request.args))
 
 
-def PercentileLines(lines_seq, percents):
-    """Returns a list with the percentile lines of the lines_seq.
-    
-    lines_seq: a list of NumPy arrays with the same shape.
-    percents: a tuple or list of the desired percentiles.
-    
-    returns: a list of NumPy arrays.
-    """
-    lines_df = pd.DataFrame(lines_seq)
-    quantiles = list() 
-    
-    for perc in percents:
-        quantiles.append( pd.DataFrame(lines_df).quantile(q=perc/100, axis=0) )
-        
-    return quantiles
-
-
-def plot_prediction(crypto, start_date=datetime(2018, 2, 1), months=3, ci=[0, 90], data_path='cryptos-py.csv'):
+@bp.route('/image_plot/<crypto>/<start_date>/<months>/<min_ci>/<max_ci>', methods=('GET',))
+def plot_prediction(crypto, start_date=datetime(2018, 2, 1), months=3, min_ci=0, max_ci=90):
     """Generates a graph with the desired crypto values on 2017 and 2018.
     Then plots the intervals of confidence for future values using the least
     squares linear regression method.
@@ -244,13 +112,17 @@ def plot_prediction(crypto, start_date=datetime(2018, 2, 1), months=3, ci=[0, 90
     start_date: datetime object, the starting date for the regression
     months: int, the number of months we want to predict
     ci: list of len 2
-    data_path: string
-
-    returns: int, the number associated to the image generated
     """
+
+    #Set parameters
+    sns.set_style('whitegrid')
     register_matplotlib_converters()
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    months = int(months)
+    ci = [int(min_ci), int(max_ci)]
 
     #Load data
+    data_path='cryptos-py.csv'
     data = pd.read_csv('cryptos-py.csv', usecols=['slug', 'date', 'num_date', 'close'])
     data = data[data.slug == crypto]
 
@@ -313,9 +185,45 @@ def plot_prediction(crypto, start_date=datetime(2018, 2, 1), months=3, ci=[0, 90
     x2 = num2date(dates.num_date.max())
     plt.axis((x1,x2,0,y2))
 
-    #Generate fig name
-    fig_code = randint(0, 999999999999)
-    fig_name = '{:012d}'.format(fig_code) + '.png'
+    # From: https://stackoverflow.com/questions/20107414/passing-a-matplotlib-figure-to-html-flask
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
 
-    plt.savefig('visual/static/images/temp/' + fig_name)
-    return fig_code
+    return send_file(img, mimetype='image/png')
+
+
+#--------------------- Functions to generate the plot ---------------------
+
+def GenerateFakeResults(original, results):
+    """Generates fake results from fake data.
+    Fake data is generated from mixing the resiudals of the fitted model.
+    Then the model is trained on this fake data.
+    
+    original: data used to generate the original results
+    results: a fitted least squares model
+    
+    returns: a fitted least squares model
+    """
+    fake = pd.DataFrame(original.num_date.copy())
+    fake['close'] = results.fittedvalues + np.random.choice(results.resid, len(results.resid))
+    formula = "close ~ num_date"
+    model = smf.ols(formula, data=fake)
+    return model.fit()
+
+
+def PercentileLines(lines_seq, percents):
+    """Returns a list with the percentile lines of the lines_seq.
+    
+    lines_seq: a list of NumPy arrays with the same shape.
+    percents: a tuple or list of the desired percentiles.
+    
+    returns: a list of NumPy arrays.
+    """
+    lines_df = pd.DataFrame(lines_seq)
+    quantiles = list() 
+    
+    for perc in percents:
+        quantiles.append( pd.DataFrame(lines_df).quantile(q=perc/100, axis=0) )
+        
+    return quantiles
